447d80b6871c73ec3c81a23d6f33241a2a554ba4
Mark Janssen <mark@sig-io.nl>
2008-02-20 16:24:09 +0000

    Only use SSLv3 as pointed out by Martin Fotak
    git-svn-id: https://proxytunnel.svn.sourceforge.net/svnroot/proxytunnel/trunk/proxytunnel@220 bc163920-b10d-0410-b2c5-a5491ca2ceef

--- ptstream.c
+++ ptstream.c
@@ -151,7 +151,7 @@ int stream_enable_ssl(PTSTREAM *pts) {
 	
 	/* Initialise the connection */
 	SSLeay_add_ssl_algorithms();
-	meth = SSLv23_client_method();
+	meth = SSLv3_client_method();
 	SSL_load_error_strings();
 
 	ctx = SSL_CTX_new (meth);
